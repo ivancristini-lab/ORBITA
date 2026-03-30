11b1ec78934a016cc3c0aa6b8d7b7521f82d7563
ivancristini-lab <ivannahuelcristini@gmail.com>
2026-03-30 00:07:34 -0300

    Delete app directory

--- app/api/insights/[contactId]/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest } from "next/server"
-import { getInsights } from "../../../../lib/store"
-import { fail, ok } from "../../../../lib/api"
-export const dynamic = "force-dynamic"
-export async function GET(_req: NextRequest, { params }: { params: { contactId: string } }) {
-  return ok({ insights: await getInsights(params.contactId) })
-}
